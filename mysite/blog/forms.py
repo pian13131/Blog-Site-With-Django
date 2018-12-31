2f5a201aaa0bf4dcbacbcac6b4946998b4c7e897
Lyu <jialun13131@hotmail.com>
2018-12-30 20:04:25 -0800

    bug free

--- mysite/blog/forms.py
+++ mysite/blog/forms.py
@@ -13,7 +13,7 @@ class PostForm(forms.ModelForm):
 		}
 
 
-class CommentForm(form.ModelForm):
+class CommentForm(forms.ModelForm):
 
 	class Meta():
 		model = Comment
--- mysite/blog/views.py
+++ mysite/blog/views.py
@@ -1,4 +1,5 @@
-from django.shortcuts import render,get_object_or_404,redirect_field_name
+from django.shortcuts import render,get_object_or_404,redirect
+from django.contrib.auth.decorators import login_required
 from django.utils import timezone
 from django.views.generic import (TemplateView,ListView,
 									DetailView,CreateView,UpdateView,
@@ -16,7 +17,7 @@ class PostListView(ListView):
 	model = Post
 
 	def get_queryset(self):
-		return Post.object.filter(published_date__lte=timezone.now()).order_by('-published_date')
+		return Post.objects.filter(published_date__lte=timezone.now()).order_by('-published_date')
 
 class PostDetailView(DetailView):
 	model = Post
@@ -39,30 +40,30 @@ class PostDeleteView(LoginRequiredMixin, DeleteView):
 
 class DraftListView(LoginRequiredMixin, ListView):
 	login_url = '/login'
-	redirect_field_name = 'blog/post_list.html'
+	redirect_field_name = 'blog/post_draft_list.html'
 	model = Post
 
 	def get_queryset(self):
-		return Post.object.filter(published_date__isnull=True).order_by('create_date')
+		return Post.objects.filter(published_date__isnull=True).order_by('created_date')
 
 ###################### Comment ######################
 
 @login_required
 def post_publish(request,pk):
 	post = get_object_or_404(Post,pk=pk)
-	post.published_date
+	post.publish()
 	return redirect('post_detail', pk=pk)
 
 @login_required
 def add_comment_to_post(request, pk):
 	post = get_object_or_404(Post, pk=pk)
-	if request.method = 'POST':
+	if request.method == 'POST':
 		form = CommentForm(request.POST)
 		if form.is_valid():
 			comment = form.save(commit=False)
 			comment.post = post
 			comment.save()
-			return redirect(post_detail,pk=post.pk)
+			return redirect('post_detail',pk=post.pk)
 
 	else:
 		form = CommentForm()
@@ -71,7 +72,7 @@ def add_comment_to_post(request, pk):
 def comment_approve(request, pk):
 	comment = get_object_or_404(Comment, pk=pk)
 	comment.approve()
-	return redirect('post_list', pk=comment.post.pk)
+	return redirect('post_detail', pk=comment.post.pk)
 
 @login_required
 def comment_remove(request,pk):
